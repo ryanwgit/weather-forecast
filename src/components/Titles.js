7ade9dbc985d0a94d0b73e8ec8933903683631e4
admin <rtwallace23@gmail.com>
2019-06-14 10:24:59 -0400

    Reuploaded.  Fixed bootstrap layout issues.  Updated with new API key.

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">React Forecast</h1>
+		<h3 className="title-container__subtitle">Find out your local temperature and conditions.</h3>
+	</div>
+);
+
+export default Titles;
